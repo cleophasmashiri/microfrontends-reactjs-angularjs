c479650a263a6876261a4bbd32904105780807d7
Cleophas Mashiri <cleophas.mashiri@standardbank.co.za>
2020-03-01 08:30:00 +0200

    updated

--- single-spa.config.js
+++ single-spa.config.js
@@ -1,6 +1,6 @@
 import {registerApplication, start} from 'single-spa';
 
-registerApplication('navbar', () => import('src/navbar/navbar.app.js').then(mod => mod.navbar), () => true);
+//registerApplication('navbar', () => import('src/navbar/navbar.app.js').then(mod => mod.navbar), () => true);
 //registerApplication('home', () => import('src/home/home.app.js'), (location) => location.pathName === '' || location.pathName === '/' || location.pathName.startWith('/home'));
 //registerApplication('angularJS', () => import('src/angularJS/angularJS.js'), (location) => location.pathName === '/angularJs');
 
